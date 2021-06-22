a555f839fd3d5e855f186495a03f7b57ee812931
ManonHofman <84331614+ManonHofman@users.noreply.github.com>
2021-06-22 15:11:07 +0200

    Add files via upload

---
+++ Les5.js
@@ -0,0 +1,78 @@
+let person = {
+    name: "Manon",
+    age: 25
+};
+
+person.name = 'Manon';
+console.log(person.age);
+
+person['age'] = 25;
+
+console.log(person.age);
+
+let evaluations [7, 10, 9]
+console.log(evaluations)
+
+let selectedColor [groen, blauw, rood, geel, 5, {greeting: "Hi ik ben een object"} ];
+selectedColor[2] = 1;
+
+console.log (selectedColor);
+console.log (array.length);
+console.log (array.groen);
+console.log (array.rood);
+
+
+
+const catBreeds = [{
+    name: "Abyssinian",
+    description: "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
+    dog_friendly: 4,
+    energy_level: 5,
+    life_span: "14 - 15",
+    origin: "Egypt",
+    temperament: ["Active", "Energetic", "Independent", "Intelligent", "Gentle"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/Abyssinian_(cat)",
+                food: {
+                        favourite_food: "fish",
+      medium_liked_food: "dried fruits", 
+      disliked_food: "walnuts"
+     }
+  },
+        {
+    name: "Aegean",
+    description: "Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding. As a breed, Aegean Cats are rare, although they are numerous on their home islands. They are generally friendly toward people and can be excellent cats for families with children.",
+    dog_friendly: 4,
+    energy_level: 53,
+    life_span: "9- 12",
+    origin: "Greece",
+    temperament: [ "Affectionate", "Social", "Intelligent", "Playful", "Active"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/Aegean_cat",
+    food: {
+                        favourite_food: "tuna",
+      medium_liked_food: "canned food", 
+      disliked_food: "all fruits"
+     }
+  },
+        {
+    name: "American Bobtail",
+    description: "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.",
+    dog_friendly: 5,
+    energy_level: 3,
+    life_span: "11 - 15",
+    origin: "United States",
+    temperament: [ "Intelligent", "Interactive", "Lively", "Playful", "Sensitive"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/American_Bobtail",
+    food: {
+                        favourite_food: "meaty things",
+      medium_liked_food: "tuna", 
+      disliked_food: "canned food"
+     }
+  }
+]
+
+console.log("Herkomst kat 3:", catBreeds[0].origin)
+console.log(energy_level kat 1)
+console.log(energy_level kat 1)
+console.log(temperament kat 2)
+console.log(temperament kat 3)
+console.log(favourite_food kat 3)
